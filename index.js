998b7fe13f0480cea46a36ac94e4034d4a6ca12e
Mostafa Salama <mostafalearning2016@gmail.com>
2018-06-13 03:34:43 +0200

    building the first release for the program , can create files and directories ' '

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const program = require('commander');
+const path = require('path') ;
+const creators = require('./src');
+program.version('0.1.0')
+.option('-e, --extension [type]','create files with the specified extension')
+.parse(process.argv);
+program.args.forEach(arg=>{
+    if(path.extname(arg)) {
+        creators.createFile(arg);
+    }
+    else {
+        if(program.extension) {
+            creators.createFile(`${arg}.${program.extension}`)
+        }
+        else {
+            creators.createDir(arg);
+        }
+    }
+})
